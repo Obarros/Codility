0b595eeb285f47f4323c2ecb562ad198d7bfb83c
obarros <ozias.barros@marionete.co.uk>
2016-12-13 22:37:34 +0000

    Defined test cases

--- src/main/scala-2.11/com/codility/challenge/_vanadium2016/Solution.scala
+++ src/main/scala-2.11/com/codility/challenge/_vanadium2016/Solution.scala
@@ -4,5 +4,8 @@ package com.codility.challenge._vanadium2016
   * Created by obarros on 13/12/2016.
   */
 object Solution {
+  def solution(s: String): Int = {
+    return 0
+  }
 
 }
--- src/test/scala-2.11/com/codility/challenge/_vanadium2016/Solution$Test.scala
+++ src/test/scala-2.11/com/codility/challenge/_vanadium2016/Solution$Test.scala
@@ -1,8 +1,30 @@
 package com.codility.challenge._vanadium2016
 
+import org.scalatest.FunSuite
+
 /**
   * Created by obarros on 13/12/2016.
   */
-class Solution$Test {
+class Solution$Test extends FunSuite {
+
+  test("testSolution 0") {
+    assert(Solution.solution("02002") == 11)
+  }
+
+  test("testSolution 1") {
+    assert(Solution.solution("0000") == 10)
+  }
+
+  test("testSolution 2") {
+    assert(Solution.solution("000") == 6)
+  }
+
+  test("testSolution 3") {
+    assert(Solution.solution("01234") == 4)
+  }
+
+  test("testSolution 4") {
+    assert(Solution.solution("0112334") == 17)
+  }
 
 }
